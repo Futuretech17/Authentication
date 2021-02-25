4d6403cd777443ea1d0477289b1151b915553ced
Benjamin <benjie@localhost.localdomain>
2021-02-25 10:49:44 +0300

    level 3 Hashing

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
-require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
-
+const md5 = require("md5");
 const app = express();
 
-console.log(process.env.API_KEY);
+
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser:true, useUnifiedTopology:true});
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true, useUnifiedTopology:true});
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"]});
 
 const User = mongoose.model("User", userSchema);
 
@@ -37,7 +33,7 @@ app.get("/register",function(req,res){
 app.post("/register", function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err){
     if(!err){
@@ -55,7 +51,7 @@ app.get("/login",function(req,res){
 app.post("/login",function(req, res){
   User.findOne({email: req.body.username}, function(err, foundUser){
     if(foundUser){
-      if (req.body.password === foundUser.password){
+      if (md5(req.body.password) === foundUser.password){
         res.render("secrets");
       }else{
         res.send("Wrong Password. Please try again");
